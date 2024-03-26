735012e0decf94f23a1a8989c9cd6da65fa25e81
JorgeLuMC <jorgecoca2323@gmail.com>
2024-03-25 21:19:53 -0400

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCW-xsqho56Nn_PIZu63JMT8EzKjrfOqYY",
+  authDomain: "scormece.firebaseapp.com",
+  projectId: "scormece",
+  storageBucket: "scormece.appspot.com",
+  messagingSenderId: "721205534337",
+  appId: "1:721205534337:web:ed196674bf2b09a449b780",
+  measurementId: "G-15N386572R"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
